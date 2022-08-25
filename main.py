491f58bde1638aa9b7b9807d1a43291c03e082e7
rohamm <rohambadi547@gmail.com>
2022-08-25 07:27:14 -0700

    add new features to endpoints

--- main.py
+++ main.py
@@ -24,3 +24,20 @@ def get_blockchain():
 
     chain = blockchain.chain
     return chain
+
+# endpoint returns the previous block
+@app.get("/previous_block/")
+def previous_block():
+    if not blockchain.is_chain_valid():
+        return fastapi.HTTPException(status_code=400,detail="the blockchain is invalid")
+    
+    return blockchain.get_previous_block()
+    
+
+# endpoint to see if blockchain is valid
+@app.get("/validate/")
+def is_blockchain_valid():
+
+    return  blockchain.is_chain_valid()
+        
+        
